e2b090d7894829409fd89e49c30450a7ac1e2910
stefan9541 <45336682+stefan9541@users.noreply.github.com>
2023-07-02 20:54:21 +0300

    add blog items

---
+++ src/constants/blogItems.ts
@@ -0,0 +1,17 @@
+export const blogItems = [
+  {
+    imgUrl: "./lemon.jpg",
+    title: "Top 10 houseplant with stable variegation",
+    nameUrl: "Top-10-houseplant-with-stable-variegation",
+    intro:
+      "Having plants in your bedroom can bring soothing and aesthetics at the same time and that's exactly what we're looking for, right?",
+  },
+  {
+    imgUrl: "./Ficus Benjamina.png",
+    title: "5 plants for your bedroom",
+    nameUrl: "5-plants-for-your-bedroom",
+    intro: "Variegated leaves come in all kinds of colours and patterns.",
+  },
+];
+
+export type BlogItemType = typeof blogItems[0];
